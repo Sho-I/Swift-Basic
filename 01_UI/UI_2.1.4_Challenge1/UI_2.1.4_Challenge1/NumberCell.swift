b12be51ca21a12767954add477ce8000c9fb6d27
Sho-I <s.ito@smartdt.jp>
2016-03-26 20:47:59 +0900

    - create project

---
+++ 01_UI/UI_2.1.4_Challenge1/UI_2.1.4_Challenge1/NumberCell.swift
@@ -0,0 +1,9 @@
+//
+//  numberCell.swift
+//  UI_2.1.4_Challenge1
+//
+//  Created by Aryzae on 2016/03/26.
+//  Copyright © 2016年 Aryzae. All rights reserved.
+//
+
+import Foundation
---
+++ 01_UI/UI_2.1.4_Challenge1/UI_2.1.4_Challenge1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI_2.1.4_Challenge1
+//
+//  Created by Aryzae on 2016/03/26.
+//  Copyright © 2016年 Aryzae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
