f5c33353fba08939274e5d0a6a0e9262df2b6fce
Sho <s.ito@smartdt.jp>
2016-04-19 23:48:47 +0900

    Revert "Issues#4"

---
+++ 01_UI/UI_2.1.4_Challenge1/UI_2.1.4_Challenge1/SymbolCell.swift
@@ -0,0 +1,13 @@
+//
+//  SymbolCell.swift
+//  UI_2.1.4_Challenge1
+//
+//  Created by Aryzae on 2016/03/26.
+//  Copyright © 2016年 Aryzae. All rights reserved.
+//
+
+import UIKit
+
+class SymbolCell: UICollectionViewCell {
+    
+}
